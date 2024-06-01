8427296b0f878aac297bc2ec794b3044fe03528a
muskan <muskana.ug21.ee@nitp.ac.in>
2024-06-01 18:49:44 +0530

    corrected

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json()); //to send res in json format (via controller)
 app.use("/images",express.static(path.join(__dirname,"/images")));// to show images in posts on frontend which is posted
 
 const corsOptions = {
-  origin: "https://blog-app-lqxf.vercel.app/",
+  origin: "https://blog-app-lqxf.vercel.app",
 //   // methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
 //   // credentials: true,
   optionsSuccessStatus: 200,
