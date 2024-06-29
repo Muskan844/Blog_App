dd3bf23bc1b4cf24cbe8f2054ef4bb749f184a26
muskan <muskana.ug21.ee@nitp.ac.in>
2024-06-29 17:29:18 +0530

    modified

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useHistory } from 'eact-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Register = () => {
   const history = useHistory();
